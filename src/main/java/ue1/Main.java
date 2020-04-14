1d231659a3007b91dccf331163e7a286de24345e
Frieda Gruse <fr.ida@gmx.de>
2020-04-14 19:15:09 +0200

    ue1 aufgabe 1

--- src/main/java/ue1/Main.java
+++ src/main/java/ue1/Main.java
@@ -1,8 +1,19 @@
 package ue1;
 
-public class Main {
+import de.htwberlin.fiw.profiler.ProfiledClass;
+import de.htwberlin.fiw.profiler.Profiler;
 
+public class Main extends ProfiledClass {
+    public void run() {
+        long ergebnis = 1;
+        for (long i = 1; i < 1000000000; i++) {
+            ergebnis += i;
+        }
+        System.out.println("Ergebnis ist: " + ergebnis);
+    }
     public static void main(String[] args) {
-        System.out.println("hello world");
+        Profiler profiler = new Profiler(Main.class);
+        profiler.start();
+        profiler.printResults();
     }
 }
