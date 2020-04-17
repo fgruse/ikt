a11e11ead25f7b0e4c1451836d52267178b7ca4e
Frieda Gruse <fr.ida@gmx.de>
2020-04-17 23:08:42 +0200

    weitere optimierung + test

---
+++ src/main/java/ue1/zweiteOptimierteEinfachVerketteteListe.java
@@ -0,0 +1,191 @@
+package ue1;
+
+public class zweiteOptimierteEinfachVerketteteListe {
+
+    /*
+    Optimierungsidee für verbesserte Laufzeit:
+    Um zu prüfen, ob die Liste noch leer ist, wird nicht mehr überprüft, ob die Länge der Liste Null ist (size == 0),
+    sondern ob der Head-Pointer bereits auf etwas zeigt (head == null). Somit spart man sich z.B. bei der
+    append- und prepend-Methode die Schleife zum ermitteln der Länge komplett.
+    Die get-Methode kann außerdem auch so umgeschrieben, dass sie die Länge der Liste nicht mehr benötigt,
+    somit fällt eine weitere Schleife weg.
+    Beim Profiling werden nur diese drei Methoden verwendet und deswegen lag mein Fokus darauf, erst einmal diese
+    zu verbessern. Die gleiche Herangehensweise kann aber auch auf einige der anderen Methoden angewandt werden.
+    */
+
+    private Element head;
+
+    public Element getHead() {
+        return head;
+    }
+
+    public zweiteOptimierteEinfachVerketteteListe() {
+        head = null;
+    }
+
+    /**
+     * Element am Anfang einfügen
+     * @param value - Element, welches eingefügt wird
+     */
+    public void prepend(int value) {
+        Element newFirstElement = new Element(value);
+        if(head==null) {
+            head = new Element();
+            head.setNext(newFirstElement);
+        }
+        else {
+            Element oldFirstElement = head.getNext();
+            head.setNext(newFirstElement);
+            newFirstElement.setNext(oldFirstElement);
+        }
+    }
+
+    /**
+     * Element am Ende einfügen
+     * @param value - Element, welches eingefügt wird
+     */
+    public void append(int value) {
+        Element newLastElement = new Element(value);
+        if(head==null) {
+            this.prepend(value);
+        }
+        else {
+            Element pointer = head.getNext();
+            while(pointer.getNext()!=null) {
+                pointer = pointer.getNext();
+            }
+            pointer.setNext(newLastElement);
+        }
+    }
+
+    /**
+     * Element an einer angegebenen Position einfügen
+     * @param value - Element, welches eingefügt wird
+     * @param index - angegebene Position
+     * @throws IndexOutOfBoundsException wenn der Index nicht vorhanden ist (<0 or >size())
+     */
+    public void insert(int index, int value) throws IndexOutOfBoundsException {
+        Element newElement = new Element(value);
+        int size = this.size();
+        if(index>size || index<0) {
+            throw new IndexOutOfBoundsException("Die Liste enthält kein Element an Index " + index + ".");
+        }
+        else {
+            if(head==null) {
+                this.prepend(value);
+            }
+            else {
+                if(index==0) {
+                    this.prepend(value); // einfügen an index 0 entspricht am anfang einfügen
+                }
+                else {
+                    Element pointer = head.getNext();
+                    for(int i=0; i<index-1; i++) {
+                        pointer = pointer.getNext(); // element, das gerade vor dem index gespeichert ist
+                    }
+                    Element oldIndex = pointer.getNext(); // element, das gerade am index gespeichert ist
+                    pointer.setNext(newElement);
+                    newElement.setNext(oldIndex);
+                }
+            }
+        }
+    }
+
+    /**
+     * Element an einer angegebenen Position abrufen
+     * @param index - angegebene Position
+     * @return value - abgerufenes Element
+     * @throws IndexOutOfBoundsException wenn der Index nicht vorhanden ist (<0 or >=size())
+     */
+    public int get(int index) throws IndexOutOfBoundsException {
+        int value;
+        if(head==null || index<0) {
+            throw new IndexOutOfBoundsException("Die Liste enthält kein Element an Index " + index + ".");
+        }
+        else if(index==0) {
+            value = head.getNext().getData();
+        }
+        else {
+            Element indexElement = head.getNext();
+            for(int i=0; i<index; i++) {
+                if(indexElement.getNext()==null) {
+                    throw new IndexOutOfBoundsException("Die Liste enthält kein Element an Index " + index + ".");
+                }
+                indexElement = indexElement.getNext();
+            }
+            value = indexElement.getData();
+        }
+        return value;
+    }
+
+    /**
+     * Element an einer angegebenen Position löschen
+     * @param index - angegebene Position
+     * @throws IndexOutOfBoundsException wenn der Index nicht vorhanden ist (<0 or >=size())
+     */
+    public void remove(int index) throws IndexOutOfBoundsException {
+        int size = this.size();
+        if(index>=size || index<0) {
+            throw new IndexOutOfBoundsException("Die Liste enthält kein Element an Index " + index + ".");
+        }
+        else {
+            Element pointer = head.getNext();
+            for(int i=0; i<index-1; i++) {
+                pointer = pointer.getNext(); // element, das gerade vor dem index gespeichert ist
+            }
+
+            if(index==0) { // erstes element
+                Element currentSecondElement = head.getNext().getNext();
+                head.setNext(currentSecondElement);
+            }
+            else if(index==(size-1)) { // letztes element
+                pointer.setNext(null);
+            }
+            else { // dazwischen
+                Element toDelete = pointer.getNext(); // element, das gerade am index gespeichert ist/ gelöscht werden soll
+                Element afterIndex = toDelete.getNext(); // element, das gerade nach dem index gespeichert ist
+                pointer.setNext(afterIndex);
+            }
+        }
+    }
+
+    /**
+     * prüft, ob das übergebene Element in der Liste enthalten ist
+     * @param value - Element, welches übergeben wird
+     * @return contains - true, wenn Element in Liste vorhanden ist, false wenn nicht
+     */
+    public boolean contains(int value){
+        int size = this.size();
+        boolean contains = false;
+        if(size>0) {
+            Element pointer = head.getNext();
+            for(int i=0; i<size; i++) {
+                if(pointer.getData()==value) {
+                    contains = true;
+                    break;
+                }
+                pointer = pointer.getNext();
+            }
+        }
+        return contains;
+    }
+
+    /**
+     * ruft die Länge der Liste ab (Hilfsmethode)
+     * @return - Länge der Liste
+     */
+    public int size() {
+        if(head==null) {
+            return 0;
+        }
+        else {
+            Element pointer = head.getNext();
+            int counter = 1;
+            while(pointer.getNext()!=null) {
+                pointer = pointer.getNext();
+                counter++;
+            }
+            return counter;
+        }
+    }
+}
---
+++ src/test/java/ue1/zweiteOptimierteEinfachVerketteteListeTest.java
@@ -0,0 +1,233 @@
+package ue1;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+public class zweiteOptimierteEinfachVerketteteListeTest {
+
+        // Testen der prepend-Methode (hinzufügen am Anfang der Liste)
+
+        @Test
+        public void testPrepend() {
+            zweiteOptimierteEinfachVerketteteListe l = new zweiteOptimierteEinfachVerketteteListe();
+
+            // bei leerer Liste
+            assertNull(l.getHead());
+            l.prepend(1);
+            assertNotNull(l.getHead());
+            assertNull(l.getHead().getData());
+            assertEquals(1, (int) l.getHead().getNext().getData());
+            assertNull(l.getHead().getNext().getNext());
+
+            // bei nicht-leerer Liste
+            l.prepend(2);
+            assertEquals(2, (int) l.getHead().getNext().getData());
+            assertEquals(1, (int) l.getHead().getNext().getNext().getData());
+            assertNull(l.getHead().getNext().getNext().getNext());
+        }
+
+        // Testen der append-Methode (hinzufügen am Ende der Liste)
+
+        @Test
+        public void testAppend() {
+            zweiteOptimierteEinfachVerketteteListe l = new zweiteOptimierteEinfachVerketteteListe();
+
+            // bei leerer Liste
+            l.append(5);
+            assertEquals(5, (int) l.getHead().getNext().getData());
+            assertNull(l.getHead().getNext().getNext());
+
+            // bei nicht-leerer Liste
+            l.append(2);
+            l.append(19);
+            assertEquals(2, (int) l.getHead().getNext().getNext().getData());
+            assertEquals(19, (int) l.getHead().getNext().getNext().getNext().getData());
+            assertNull(l.getHead().getNext().getNext().getNext().getNext());
+        }
+
+        // Testen der insert-Methode (einfügen an einem bestimmten Index)
+
+        @Test
+        public void testInsert() {
+            zweiteOptimierteEinfachVerketteteListe l = new zweiteOptimierteEinfachVerketteteListe();
+
+            // bei leerer Liste
+            l.insert(0, 5);
+            assertEquals(1, l.size());
+            assertEquals(5, (int) l.getHead().getNext().getData());
+            assertNull(l.getHead().getNext().getNext());
+
+            // bei nicht-leerer Liste, einfügen bei Index 0
+            l.insert(0, 6);
+            assertEquals(2, l.size());
+            assertEquals(6, (int) l.getHead().getNext().getData());
+            assertEquals(5, (int) l.getHead().getNext().getNext().getData());
+
+            // bei nicht-leerer Liste, zwischendrin einfügen
+            l.insert(1, 7);
+            assertEquals(3, l.size());
+            assertEquals(6, (int) l.getHead().getNext().getData());
+            assertEquals(7, (int) l.getHead().getNext().getNext().getData());
+            assertEquals(5, (int) l.getHead().getNext().getNext().getNext().getData());
+        }
+
+        @Test(expected = IndexOutOfBoundsException.class)
+        public void testInsertAtIndexOutOfBoundsNegative() {
+            zweiteOptimierteEinfachVerketteteListe l = new zweiteOptimierteEinfachVerketteteListe();
+            l.append(5);
+            l.append(2);
+            l.append(19);
+            l.insert(-1, 4);
+        }
+
+        @Test(expected = IndexOutOfBoundsException.class)
+        public void testInsertAtIndexOutOfBoundsTooBig() {
+            zweiteOptimierteEinfachVerketteteListe l = new zweiteOptimierteEinfachVerketteteListe();
+            l.append(5);
+            l.append(2);
+            l.append(19);
+            l.insert(100, 4);
+        }
+
+        @Test(expected = IndexOutOfBoundsException.class)
+        public void testaInsertAtIndexOutOfBoundsTooBigEmptyList() {
+            zweiteOptimierteEinfachVerketteteListe l = new zweiteOptimierteEinfachVerketteteListe();
+            l.insert(3, 4);
+        }
+
+        // Testen der get-Methode
+
+        @Test
+        public void testGet() {
+            zweiteOptimierteEinfachVerketteteListe l = new zweiteOptimierteEinfachVerketteteListe();
+            l.append(5);
+            l.append(2);
+            l.append(19);
+            l.append(2);
+            assertEquals(5, l.get(0));
+            assertEquals(2, l.get(1));
+            assertEquals(19, l.get(2));
+            assertEquals(2, l.get(3));
+        }
+
+        @Test(expected = IndexOutOfBoundsException.class)
+        public void testGetIndexOutOfBoundsNegative() {
+            zweiteOptimierteEinfachVerketteteListe l = new zweiteOptimierteEinfachVerketteteListe();
+            l.append(5);
+            l.append(2);
+            l.append(19);
+            l.append(2);
+            l.get(-100);
+        }
+
+        @Test(expected = IndexOutOfBoundsException.class)
+        public void testGetIndexOutOfBoundsTooBig() {
+            zweiteOptimierteEinfachVerketteteListe l = new zweiteOptimierteEinfachVerketteteListe();
+            l.append(5);
+            l.append(2);
+            l.append(19);
+            l.append(2);
+            l.get(100);
+        }
+
+        @Test(expected = IndexOutOfBoundsException.class)
+        public void testGetIndexOutOfBoundsTooBigEmptyList() {
+            zweiteOptimierteEinfachVerketteteListe l = new zweiteOptimierteEinfachVerketteteListe();
+            l.get(100);
+        }
+
+        // Testen der remove-Methode an einem bestimmten Index
+
+        @Test
+        public void testRemove() {
+            zweiteOptimierteEinfachVerketteteListe l = new zweiteOptimierteEinfachVerketteteListe();
+            l.append(5);
+            l.append(2);
+            l.append(19);
+            l.append(7);
+            l.remove(1);
+            assertEquals(3 , l.size());
+            assertEquals(5, (int) l.getHead().getNext().getData());
+            assertEquals(19, (int) l.getHead().getNext().getNext().getData());
+            assertEquals(7, (int) l.getHead().getNext().getNext().getNext().getData());
+        }
+
+        @Test
+        public void testRemoveAtIndex0() {
+            zweiteOptimierteEinfachVerketteteListe l = new zweiteOptimierteEinfachVerketteteListe();
+            l.append(5);
+            l.append(2);
+            l.append(19);
+            l.append(2);
+            l.remove(0);
+            assertEquals(3 , l.size());
+            assertEquals(2, (int) l.getHead().getNext().getData());
+        }
+
+        @Test
+        public void testRemoveAtLastIndex() {
+            zweiteOptimierteEinfachVerketteteListe l = new zweiteOptimierteEinfachVerketteteListe();
+            l.append(5);
+            l.append(2);
+            l.append(19);
+            l.append(2);
+            l.remove(3);
+            assertEquals(3 , l.size());
+            assertEquals(19, (int) l.getHead().getNext().getNext().getNext().getData());
+        }
+
+        @Test(expected = IndexOutOfBoundsException.class)
+        public void testRemoveAtIndexOutOfBoundsNegative() {
+            zweiteOptimierteEinfachVerketteteListe l = new zweiteOptimierteEinfachVerketteteListe();
+            l.append(5);
+            l.append(2);
+            l.append(19);
+            l.append(2);
+            l.remove(-100);
+        }
+
+        @Test(expected = IndexOutOfBoundsException.class)
+        public void testRemoveAtIndexOutOfBoundsTooBig() {
+            zweiteOptimierteEinfachVerketteteListe l = new zweiteOptimierteEinfachVerketteteListe();
+            l.append(5);
+            l.append(2);
+            l.append(19);
+            l.append(2);
+            l.remove(100);
+        }
+
+        // Testen der contains-Methode
+
+        @Test
+        public void testContains() {
+            zweiteOptimierteEinfachVerketteteListe l = new zweiteOptimierteEinfachVerketteteListe();
+            l.append(5);
+            l.append(2);
+            l.append(19);
+            assertTrue(l.contains(5));
+            assertTrue(l.contains(2));
+            assertTrue(l.contains(19));
+            assertFalse(l.contains(100));
+        }
+
+        // Testen der size-Methode
+
+        @Test
+        public void testSize() {
+            zweiteOptimierteEinfachVerketteteListe l = new zweiteOptimierteEinfachVerketteteListe();
+
+            // bei leerer Liste
+            assertEquals(0, l.size());
+
+            //bei nicht-leerer Liste
+            l.append(5);
+            l.append(2);
+            l.append(19);
+            assertEquals(3, l.size());
+        }
+}
