4732d37c5ea8fe46d86ee7e21b19336760063888
Frieda Gruse <fr.ida@gmx.de>
2020-05-13 16:47:08 +0200

    set implementation + profiler 1

---
+++ src/main/java/ue2/ProfilerSet.java
@@ -0,0 +1,32 @@
+package ue2;
+
+import de.htwberlin.fiw.profiler.ProfiledClass;
+import java.util.Random;
+
+public class ProfilerSet extends ProfiledClass {
+
+    @Override
+    public void run() {
+        Set set = new Set();
+        Random r = new Random();
+        for(int i=0; i<10000; i++) {
+            set.add(r.nextInt());
+        }
+        System.out.println("- - - - - - - - - - - - - - - - - - -");
+        System.out.println("Anfang bis Ende:");
+        Element pointer = set.getList().getHead();
+        while (pointer.getNext().getData() != null) {
+            pointer = pointer.getNext();
+            System.out.println(pointer.getData());
+        }
+        System.out.println("- - - - - - - - - - - - - - - - - - -");
+        System.out.println("Ende bis Anfang:");
+        Element pointerTail = set.getList().getTail();
+        while (pointerTail.getPrev().getData() != null) {
+            pointerTail = pointerTail.getPrev();
+            System.out.println(pointerTail.getData());
+        }
+        System.out.println("- - - - - - - - - - - - - - - - - - -");
+        System.out.println("Anzahl der Elemente: " + set.size());
+    }
+}
