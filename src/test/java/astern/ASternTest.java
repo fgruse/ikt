a87093e17a2f07aced99d2f9d4a9cd9a3cb238dc
Frieda Gruse <fr.ida@gmx.de>
2020-06-18 11:25:29 +0200

    added more testcases

--- src/test/java/astern/ASternTest.java
+++ src/test/java/astern/ASternTest.java
@@ -8,7 +8,7 @@ public class ASternTest {
 
     @Test
     public void testcase1() {
-        AStern a = new AStern("ortschaften-demo.csv", 0, 1);
+        AStern a = new AStern("src/main/java/astern/ortschaften-demo.csv", 0, 1);
         final Knoten[] weg = a.computeShortestPath();
         assertEquals(0, weg[0].getIndex());
         assertEquals(4404, weg[1].getIndex());
@@ -26,7 +26,7 @@ public class ASternTest {
 
     @Test
     public void testcase2() {
-        AStern a = new AStern("ortschaften-demo.csv", 0, 9999);
+        AStern a = new AStern("src/main/java/astern/ortschaften-demo.csv", 0, 9999);
         final Knoten[] weg = a.computeShortestPath();
         assertEquals(0, weg[0].getIndex());
         assertEquals(5960, weg[1].getIndex());
@@ -45,7 +45,7 @@ public class ASternTest {
 
     @Test
     public void testcase3() {
-        AStern a = new AStern("ortschaften-demo.csv", 2000, 3000);
+        AStern a = new AStern("src/main/java/astern/ortschaften-demo.csv", 2000, 3000);
         final Knoten[] weg = a.computeShortestPath();
         assertEquals(2000, weg[0].getIndex());
         assertEquals(3237, weg[1].getIndex());
@@ -62,7 +62,7 @@ public class ASternTest {
 
     @Test
     public void testcase4() {
-        AStern a = new AStern("ortschaften-demo.csv", 3361, 9261);
+        AStern a = new AStern("src/main/java/astern/ortschaften-demo.csv", 3361, 9261);
         final Knoten[] weg = a.computeShortestPath();
         assertEquals(3361, weg[0].getIndex());
         assertEquals(380, weg[1].getIndex());
@@ -85,5 +85,78 @@ public class ASternTest {
         assertEquals(4203, weg[18].getIndex());
         assertEquals(9790, weg[19].getIndex());
         assertEquals(9261, weg[20].getIndex());
+    }
+
+    @Test
+    public void testcase5() {
+        AStern a = new AStern("src/main/java/astern/ortschaften-demo.csv", 20, 505);
+        final Knoten[] weg = a.computeShortestPath();
+        StringBuilder wegString = new StringBuilder();
+        for(final Knoten knoten : weg) {
+            wegString.append(knoten.getIndex()).append(" ");
+        }
+        assertEquals("20 4920 505 ", wegString.toString());
+    }
+
+    @Test
+    public void testcase6() {
+        AStern a = new AStern("src/main/java/astern/ortschaften-demo.csv", 3243, 5875);
+        final Knoten[] weg = a.computeShortestPath();
+        StringBuilder wegString = new StringBuilder();
+        for(final Knoten knoten : weg) {
+            wegString.append(knoten.getIndex()).append(" ");
+        }
+        assertEquals("3243 404 6757 6849 2499 447 7022 4750 7017 5875 ", wegString.toString());
+    }
+
+    @Test
+    public void testcase7() {
+        AStern a = new AStern("src/main/java/astern/ortschaften-demo.csv", 200, 801);
+        final Knoten[] weg = a.computeShortestPath();
+        StringBuilder wegString = new StringBuilder();
+        for(final Knoten knoten : weg) {
+            wegString.append(knoten.getIndex()).append(" ");
+        }
+        assertEquals("200 7859 7713 2955 4567 2331 3315 2062 7523 801 ", wegString.toString());
+    }
+
+    @Test
+    public void testcase8() {
+        AStern a = new AStern("src/main/java/astern/ortschaften-demo.csv", 90, 1111);
+        final Knoten[] weg = a.computeShortestPath();
+        StringBuilder wegString = new StringBuilder();
+        for(final Knoten knoten : weg) {
+            wegString.append(knoten.getIndex()).append(" ");
+        }
+        assertEquals("90 4441 5872 8024 129 1008 887 2826 1890 4794 8964 1111 ", wegString.toString());
+    }
+
+    @Test
+    public void testcase9() {
+        AStern a = new AStern("src/main/java/astern/ortschaften-demo.csv", 9900, 3420);
+        final Knoten[] weg = a.computeShortestPath();
+        StringBuilder wegString = new StringBuilder();
+        for(final Knoten knoten : weg) {
+            wegString.append(knoten.getIndex()).append(" ");
+        }
+        assertEquals("9900 3678 9532 3236 5971 2192 1286 2319 3687 3560 1637 6186 5818 1512 6886 3420 ", wegString.toString());
+    }
+
+    @Test
+    public void testcase10() {
+        AStern a = new AStern("src/main/java/astern/ortschaften-demo-klein.csv", 0, 1);
+        final Knoten[] weg = a.computeShortestPath();
+        assertEquals(0, weg.length);
+    }
+
+    @Test
+    public void testcase11() {
+        AStern a = new AStern("src/main/java/astern/ortschaften-demo-klein.csv", 3, 2);
+        final Knoten[] weg = a.computeShortestPath();
+        StringBuilder wegString = new StringBuilder();
+        for(final Knoten knoten : weg) {
+            wegString.append(knoten.getIndex()).append(" ");
+        }
+        assertEquals("3 1 2 ", wegString.toString());
     }
 }
