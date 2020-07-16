d2cc7edd4958a3e74c9525fc2db3353224cb230e
Frieda Gruse <fr.ida@gmx.de>
2020-07-16 18:54:26 +0200

    javadoc main

--- src/main/java/astern/Main.java
+++ src/main/java/astern/Main.java
@@ -3,13 +3,25 @@ package astern;
 import de.htwberlin.fiw.profiler.ProfiledClass;
 import de.htwberlin.fiw.profiler.Profiler;
 
+/**
+ * Diese Klasse stellt den hauptsächlichen Eingangspunkt für das Programm dar und implementiert den Profiler.
+ */
 public class Main extends ProfiledClass {
+
+    /**
+     * Main-Methode, ruft Profiler auf und gibt Ergebnisse in der Konsole aus
+     * @param args ungenutzt
+     */
     public static void main(final String[] args) {
         final Profiler profiler = new Profiler(Main.class);
         profiler.start();
         profiler.printResults();
     }
 
+    /**
+     * Implementiert run-Methode des Profilers, erzeugt Graph aus Eingabedatei, berechnet kürzesten Weg zwischen zwei Knoten
+     * und gibt diesen und dessen Länge in der Konsole aus, falls er gefunden wurde
+     */
     @Override
     public void run() {
         final String FILE_NAME = "src/main/java/astern/ortschaften-demo.csv";
@@ -31,3 +43,8 @@ public class Main extends ProfiledClass {
         }
     }
 }
+
+
+
+
+
